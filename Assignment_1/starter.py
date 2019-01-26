b0e33ad4016d8f00c207b9fa00208b2317851d8f
Jinyi <jinyi.chen@mail.utoronto.ca>
2019-01-26 14:41:32 -0500

    mse function

--- Assignment_1/starter.py
+++ Assignment_1/starter.py
@@ -21,21 +21,44 @@ def loadData():
         testData, testTarget = Data[3600:], Target[3600:]
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
+#MSE is the sum of squared distances between our target variable and predicted values.
 def MSE(W, b, x, y, reg):
+    #f(w) = 1/2N * ||xw-y||^2 + reg/2 *||w||^2
+    #y_predicted definition
+    y_predicted = tf.matmul(x,W)+b
+
+    #error definition
+    N = y.shape[1]
+    mean_uqared_error = tf.reduce_sum((1/(2*N))*tf.square(y_predicted - y)+(reg/2)*tf.square(W), name = 'mean_uqared_error')
+    return mean_uqared_error
+
+
+#def gradMSE(W, b, x, y, reg):
+
+
+#def crossEntropyLoss(W, b, x, y, reg):
     # Your implementation here
 
-def gradMSE(W, b, x, y, reg):
+#def gradCE(W, b, x, y, reg):
     # Your implementation here
 
-def crossEntropyLoss(W, b, x, y, reg):
+#def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
     # Your implementation here
 
-def gradCE(W, b, x, y, reg):
+#def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
     # Your implementation here
 
-def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
-    # Your implementation here
+if __name__ == '__main__':
+    #initialize session
+    init = tf.global_variables_initializer()
 
-def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
-    # Your implementation here
+    sess = tf.InteractiveSession()
+    sees.run(init)
 
+    #training the model
+    
+    #for step in range(0,200):
+        _, err, currentW, currentb, yhat = sess.run()
+    #trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    #mse()
+    #return
