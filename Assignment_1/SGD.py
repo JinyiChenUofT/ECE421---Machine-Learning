3d51a0c900608ab5064a4b99328ebfa7390c6f2f
Jinyi <jinyi.chen@mail.utoronto.ca>
2019-02-05 19:06:05 -0500

    something

--- Assignment_1/SGD.py
+++ Assignment_1/SGD.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from starter import buildGraph, loadData
 
+
 def trainModel(lossType=None):
 
     # Initialize session
     #X = X.reshape(3500,784)
-                
+
     trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
-    trainData = trainData.reshape(3500,784)
+    trainData = trainData.reshape(3500, 784)
     W, b, X, y_target, y_predicted, loss, train = buildGraph(lossType=lossType)
     init = tf.global_variables_initializer()
     sess = tf.InteractiveSession()
@@ -18,70 +19,84 @@ def trainModel(lossType=None):
     sess.run(init)
 
     # Training the model
-    
+
     batch_size = 500
-    for i in range (700):
+    for i in range(700):
 
         randIndx = np.arange(len(trainData))
         np.random.shuffle(randIndx)
         trainData, trainTarget = trainData[randIndx], trainTarget[randIndx]
 
+        
         for step in range(7):
             offset = (step * batch_size) % (len(trainData) - batch_size)
             # Generate a minibatch.
             batch_data = trainData[offset:(offset + batch_size), :]
             batch_labels = trainTarget[offset:(offset + batch_size), :]
-                        
-            _, err, currentW, currentb, predictions = sess.run([train, loss, W, b, y_predicted], feed_dict={X: batch_data, y_target: batch_labels}) 
-            #if step % 10 == 0:
+
+            _, err, currentW, currentb, predictions = sess.run(
+                [train, loss, W, b, y_predicted], feed_dict={X: batch_data, y_target: batch_labels})
+            # if step % 10 == 0:
             #        print("Iteration: %d, MSE-training: %.2f" %(step, err))
-        
-    return currentW, currentb, predictions   
+
+    return currentW, currentb, predictions
     # Final testing error
     #errTest = sess.run(meanSquaredError, feed_dict = {X: testData, y_target: testTarget})
     #print("Final Testing MSE: %.2f:" % (errTest))
-def classify_result(Data, Target, W,b):
-    N=Target.shape[0]
-    classifier=np.zeros((N,1))
-    predict=np.dot(Data,W)+b
-    loss=0
-  
+
+
+def classify_result(Data, Target, W, b, LossType):
+    N = Target.shape[0]
+    predict = 0
+    classifier = np.zeros((N, 1))
+    if (LossType == "MSE"):
+        predict = (tf.matmul(Data, W) + b)
+    elif (LossType == "CE"):
+        logits = (tf.matmul(Data, W) + b)
+        predict = tf.sigmoid(logits)
+    loss = 0
+
     for i in range(N):
-        if predict[i]>0.5:
-           classifier[i]=1
+        if predict[i] > 0.5:
+            classifier[i] = 1
         else:
-           classifier[i]=0
-      
-        if Target[i]!=classifier[i]:
-           loss=loss+1
-      
+            classifier[i] = 0
+
+        if Target[i] != classifier[i]:
+            loss = loss+1
+
     acc = 1 - loss/N
     print(classifier.shape)
     print(Target.shape)
     return classifier, loss, acc
 
-def accuracy(predictions, labels):
-    
-   return (100.00* np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))/predictions.shape[0])
-         
 
 '''
 def accuracy(predictions, labels):
-        return (np.sum((predictions>=0.5)==labels) / np.shape(predictions)[0])
+    
+   return (100.00* np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))/predictions.shape[0])
 '''
+
+
+def accuracy(predictions, labels):
+    print (predictions.shape)
+    print (labels.shape)
+    return (np.sum((predictions >= 0.5) == labels) / np.shape(predictions)[0])
+
+
 if __name__ == '__main__':
-        W, b, predictions = trainModel("CE")
-        trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
-        validData = validData.reshape(100,784)
-        trainData = trainData.reshape(3500,784)
-        #accura = accuracy(predictions,trainTarget)
-        #print (accura)
-        #classifier,loss = classify_result(validData, validTarget, W,b)
-        classifier,loss,acc = classify_result(trainData, trainTarget, W,b)
-        print ("done")
-        print (loss)
-        print (acc)
-        '''
+    W, b, predictions = trainModel("MSE")
+    trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    validData = validData.reshape(100, 784)
+    trainData = trainData.reshape(3500, 784)
+    
+    #print(train_acc)
+    #classifier,loss = classify_result(validData, validTarget, W,b)
+    #classifier,loss,acc = classify_result(trainData, trainTarget, W,b,"CE")
+    #print ("done")
+    #print (loss)
+    #print (acc)
+    '''
         classifier,loss = classify_result(trainData, trainTarget, W,b)
         print ("done")
         print(classifier)
--- Assignment_1/starter.py
+++ Assignment_1/starter.py
@@ -4,21 +4,24 @@ import matplotlib.pyplot as plt
 from numpy import linalg as LA
 import json
 from enum import Enum
+
+
 class ModelType(Enum):
     TrainModel = 1
     ValidationModel = 2
     TestModel = 3
 
+
 def loadData():
-    with np.load('notMNIST.npz') as data :
-        Data, Target = data ['images'], data['labels']
+    with np.load('notMNIST.npz') as data:
+        Data, Target = data['images'], data['labels']
         posClass = 2
         negClass = 9
-        dataIndx = (Target==posClass) + (Target==negClass)
+        dataIndx = (Target == posClass) + (Target == negClass)
         Data = Data[dataIndx]/255.
         Target = Target[dataIndx].reshape(-1, 1)
-        Target[Target==posClass] = 1
-        Target[Target==negClass] = 0
+        Target[Target == posClass] = 1
+        Target[Target == negClass] = 0
         np.random.seed(421)
         randIndx = np.arange(len(Data))
         np.random.shuffle(randIndx)
@@ -28,20 +31,22 @@ def loadData():
         testData, testTarget = Data[3600:], Target[3600:]
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
-#MSE is the sum of squared distances between our target variable and predicted values.
+# MSE is the sum of squared distances between our target variable and predicted values.
+
+
 def MSE(W, b, x, y, reg):
-    #f(w) = 1/2N * ||xw-y||^2 + reg/2 *||w||^2
-    #y_predicted definition
+    # f(w) = 1/2N * ||xw-y||^2 + reg/2 *||w||^2
+    # y_predicted definition
     W_tranpose = np.transpose(W)
-    y_predicted = np.matmul(x,W_tranpose)+b
-    #loss term
+    y_predicted = np.matmul(x, W_tranpose)+b
+    # loss term
     N = y.shape[0]
     loss_term = (1/(2*N))*np.sum(np.square(y_predicted-y))
 
-    #regularization term
-    reg_term = (reg/2)*np.sum(np.matmul(W,W_tranpose))
-    
-    #loss function definition
+    # regularization term
+    reg_term = (reg/2)*np.sum(np.matmul(W, W_tranpose))
+
+    # loss function definition
     mean_suqared_error = loss_term+reg_term
     return mean_suqared_error
 
@@ -49,18 +54,19 @@ def MSE(W, b, x, y, reg):
 def gradMSE(W, b, x, y, reg):
     N = y.shape[0]
     W_tranpose = np.transpose(W)
-    y_predicted = np.matmul(x,W_tranpose)+b
+    y_predicted = np.matmul(x, W_tranpose)+b
 
-    #gradient with respect to weight
-    grad_w_loss = (1/N)*np.dot(np.transpose(x),(y_predicted-y))
+    # gradient with respect to weight
+    grad_w_loss = (1/N)*np.dot(np.transpose(x), (y_predicted-y))
     grad_w_loss = np.transpose(grad_w_loss)
-    grad_w_reg= reg*W
+    grad_w_reg = reg*W
     grad_w = grad_w_loss + grad_w_reg
 
-    #gradient with respect to bias
+    # gradient with respect to bias
     grad_b = (1/N)*np.sum(y_predicted-y)
-    
+
     return grad_w, grad_b
+
 
 '''
 def grad_descent(W, b, x,y, alpha, epochs, reg, error_tol):
@@ -91,11 +97,13 @@ def grad_descent(W, b, x,y, alpha, epochs, reg, error_tol):
     #plt.legend()
     plt.show()
 '''
-#def grad_descent(W, b, x, y, alpha, epochs, reg, error_tol, model):
-def grad_descent(W,b,train_x, train_y, val_x,val_y, test_x, test_y, alpha, epochs, reg, error_tol):
+# def grad_descent(W, b, x, y, alpha, epochs, reg, error_tol, model):
+
+
+def grad_descent(W, b, train_x, train_y, val_x, val_y, test_x, test_y, alpha, epochs, reg, error_tol):
     i = 0
     W1 = W2 = W3 = W
-    b1 = b2 = b3 =b
+    b1 = b2 = b3 = b
     train_loss_set1 = []
     train_loss_set2 = []
     train_loss_set3 = []
@@ -109,55 +117,51 @@ def grad_descent(W,b,train_x, train_y, val_x,val_y, test_x, test_y, alpha, epoch
     alpha2 = 0.001
     alpha3 = 0.0001
 
-    #while i < epochs and abs(np.linalg.norm(w_step_size))<=error_tol:
+    # while i < epochs and abs(np.linalg.norm(w_step_size))<=error_tol:
     while i < epochs:
-        grad_w1, grad_b1  = gradMSE(W1,b1,train_x,train_y,reg)
-        grad_w2, grad_b2  = gradMSE(W2,b2,val_x,val_y,reg)
-        grad_w3, grad_b3  = gradMSE(W3,b3,test_x,test_y,reg)
+        grad_w1, grad_b1 = gradMSE(W1, b1, train_x, train_y, reg)
+        grad_w2, grad_b2 = gradMSE(W2, b2, val_x, val_y, reg)
+        grad_w3, grad_b3 = gradMSE(W3, b3, test_x, test_y, reg)
 
         w_step_size1 = alpha * grad_w1
         b_step_size1 = alpha * grad_b1
         W1 = W1 - w_step_size1
         b1 = b1 - b_step_size1
-        
+
         w_step_size2 = alpha2 * grad_w2
         b_step_size2 = alpha2 * grad_b2
         W2 = W2 - w_step_size2
         b2 = b2 - b_step_size2
-                
+
         w_step_size3 = alpha3 * grad_w3
         b_step_size3 = alpha3 * grad_b3
         W3 = W3 - w_step_size3
         b3 = b3 - b_step_size3
-            
-        train_mse1 = MSE(W1,b1,train_x,train_y,reg)
-        val_mse1 = MSE(W1,b1,val_x,val_y,reg)
-        test_mse1 = MSE(W1,b1,test_x,test_y,reg)
+
+        train_mse1 = MSE(W1, b1, train_x, train_y, reg)
+        val_mse1 = MSE(W1, b1, val_x, val_y, reg)
+        test_mse1 = MSE(W1, b1, test_x, test_y, reg)
         train_loss_set1.append(train_mse1)
         val_loss_set1.append(val_mse1)
         test_loss_set1.append(test_mse1)
 
-        train_mse2 = MSE(W2,b2,val_x,val_y,reg)
-        val_mse2 = MSE(W2,b2,val_x,val_y,reg)
-        test_mse2 = MSE(W2,b2,test_x,test_y,reg)
+        train_mse2 = MSE(W2, b2, val_x, val_y, reg)
+        val_mse2 = MSE(W2, b2, val_x, val_y, reg)
+        test_mse2 = MSE(W2, b2, test_x, test_y, reg)
         train_loss_set2.append(train_mse2)
         val_loss_set2.append(val_mse2)
         test_loss_set2.append(test_mse2)
 
-        train_mse3 = MSE(W3,b3,train_x,train_y,reg)
-        val_mse3 = MSE(W3,b3,val_x,val_y,reg)
-        test_mse3 = MSE(W3,b3,test_x,test_y,reg)
+        train_mse3 = MSE(W3, b3, train_x, train_y, reg)
+        val_mse3 = MSE(W3, b3, val_x, val_y, reg)
+        test_mse3 = MSE(W3, b3, test_x, test_y, reg)
         train_loss_set3.append(train_mse3)
         val_loss_set3.append(val_mse3)
         test_loss_set3.append(test_mse3)
 
-
         i = i + 1
-    
 
-    
-    
-    print ("Trainning model\n")
+    print("Trainning model\n")
     plt.figure(1)
     plt.title("trainning loss set: ephocs=5000 reg=0")
     plt.plot(train_loss_set1)
@@ -167,9 +171,10 @@ def grad_descent(W,b,train_x, train_y, val_x,val_y, test_x, test_y, alpha, epoch
     plt.savefig(pic_name)
     plt.xlabel("epochs")
     plt.ylabel("loss")
-    plt.legend(['alpha=0.005','alpha=0.001','alpha=0.0001'],loc='upper right')
-    
-    print ("Validation model")
+    plt.legend(['alpha=0.005', 'alpha=0.001',
+                'alpha=0.0001'], loc='upper right')
+
+    print("Validation model")
     plt.figure(2)
     plt.title("Validation loss set: ephocs=5000 reg=0")
     plt.plot(val_loss_set1)
@@ -179,9 +184,10 @@ def grad_descent(W,b,train_x, train_y, val_x,val_y, test_x, test_y, alpha, epoch
     plt.savefig(pic_name)
     plt.xlabel("epochs")
     plt.ylabel("loss")
-    plt.legend(['alpha=0.005','alpha=0.001','alpha=0.0001'],loc='upper right')
-    
-    print ("Test model")
+    plt.legend(['alpha=0.005', 'alpha=0.001',
+                'alpha=0.0001'], loc='upper right')
+
+    print("Test model")
     plt.figure(3)
     plt.title("Test loss set: ephocs=5000 reg=0")
     plt.plot(test_loss_set1)
@@ -191,7 +197,8 @@ def grad_descent(W,b,train_x, train_y, val_x,val_y, test_x, test_y, alpha, epoch
     plt.savefig(pic_name)
     plt.xlabel("epochs")
     plt.ylabel("loss")
-    plt.legend(['alpha=0.005','alpha=0.001','alpha=0.0001'],loc='upper right')
+    plt.legend(['alpha=0.005', 'alpha=0.001',
+                'alpha=0.0001'], loc='upper right')
     plt.show()
     '''
     if (model == ModelType.TrainModel):
@@ -225,9 +232,9 @@ def grad_descent(W,b,train_x, train_y, val_x,val_y, test_x, test_y, alpha, epoch
         plt.ylabel("loss")
         plt.show()
     '''
-    
 
     return W, b
+
 
 '''
 def plot_loss(W, b, train_x, train_y, val_x,val_y, test_x, test_y, alpha, epochs, reg, error_tol):
@@ -236,64 +243,69 @@ def plot_loss(W, b, train_x, train_y, val_x,val_y, test_x, test_y, alpha, epochs
     val_w, val_b = grad_descent(train_w, train_b, val_x, val_y, alpha, epochs, reg, error_tol, ModelType.ValidationModel)
     grad_descent(val_w, val_b, test_x, test_y, alpha, epochs, reg, error_tol, ModelType.TestModel)
 '''
+
+
 def crossEntropyLoss(W, b, x, y, reg):
     N = y.shape[0]
     W_tranpose = np.transpose(W)
-    y_hat = 1/(1+np.exp(np.matmul(x,W_tranpose)+b))
-    
-    #loss term
+    y_hat = 1/(1+np.exp(np.matmul(x, W_tranpose)+b))
+
+    # loss term
     loss_term = np.mean(-y*np.log(y_hat)-(1-y)*np.log(1-y_hat))
 
-    #regularization term
-    reg_term = (reg/2)*np.sum(np.matmul(W,W_tranpose))
+    # regularization term
+    reg_term = (reg/2)*np.sum(np.matmul(W, W_tranpose))
 
     CE = loss_term+reg_term
     return CE
 
 
-#def gradCE(W, b, x, y, reg):
-def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
-#def buildGraph(lossType=None):
-    
-    W = tf.Variable(tf.truncated_normal(shape=[784,1], stddev=0.5), name='weights')
+# def gradCE(W, b, x, y, reg):
+def buildGraph(beta=None, epsilon=None, lossType=None, learning_rate=None):
+    # def buildGraph(lossType=None):
+
+    W = tf.Variable(tf.truncated_normal(
+        shape=[784, 1], stddev=0.5), name='weights')
     b = tf.Variable(tf.zeros(1), name='biases')
     X = tf.placeholder(tf.float32, [None, 784], name='input_x')
-    y_target = tf.placeholder(tf.float32, [None,1], name='target_y')
-
+    y_target = tf.placeholder(tf.float32, [None, 1], name='target_y')
+    beta = 0
     # Graph definition
-    y_predicted = tf.matmul(X,W) + b
+    y_predicted = tf.matmul(X, W) + b
+    reg_term = beta * tf.nn.l2_loss(W)/2
     tf.set_random_seed(421)
     loss = 0.0
-
+    
     if lossType == "MSE":
-        loss = tf.reduce_mean(tf.reduce_mean(tf.square(y_predicted - y_target), 
-                                                reduction_indices=1, 
-                                                name='squared_error'), 
-                                  name='mean_squared_error')
-        loss = loss/2
+        loss_term = tf.reduce_mean(tf.reduce_mean(tf.square(y_predicted - y_target),
+                                                  reduction_indices=1,
+                                                  name='squared_error'),
+                                   name='mean_squared_error')
+        loss = loss_term/2 + reg_term
+        
 
     elif lossType == "CE":
-        y_hat = 1/(1+tf.exp(tf.matmul(X,W)+b))
-        loss = tf.reduce_mean(-y_target*tf.log(y_hat)-(1-y_target)*tf.log(1-y_hat),
-                                  name='cross_entropy_loss')
-        loss = loss/2
+        logits = tf.matmul(X, W) + b
+        y_hat = tf.sigmoid(logits)
+        loss_term = tf.losses.sigmoid_cross_entropy(y_target, y_hat)
+        loss = loss + reg_term
 
-    optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
     train = optimizer.minimize(loss)
-
+    
     
     return W, b, X, y_target, y_predicted, loss, train
 
 
 def accuracy(predictions, labels):
-  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
-          / predictions.shape[0])
+    return (np.sum((predictions >= 0.5) == labels) / 500)
 
-#def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
+# def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
     # Your implementation here
 
-#def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
+# def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
     # Your implementation here
+
 
 if __name__ == '__main__':
     '''
@@ -302,26 +314,26 @@ if __name__ == '__main__':
 
     sess = tf.InteractiveSession()
     sees.run(init)
-    
+
 
     #training the model
-    
+
     #for step in range(0,200):
         _, err, currentW, currentb, yhat = sess.run()
     '''
-    
+
     trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
-    #print (trainData.shape) (3500, 28, 28) 28*28 = 784
+    # print (trainData.shape) (3500, 28, 28) 28*28 = 784
     #print (validData.shape) (100, 28, 28)
     #print (testData.shape) (145, 28, 28)
-    
+
     #print (trainTarget.shape)
     #print (trainTarget.shape[1])
     #print (testData.shape)
     #print (validData.shape)
-    train_X = trainData.reshape(3500,784)
-    val_X = validData.reshape(100,784)
-    test_X = testData.reshape(145,784)
+    train_X = trainData.reshape(3500, 784)
+    val_X = validData.reshape(100, 784)
+    test_X = testData.reshape(145, 784)
 
     W = np.zeros((1, 784))
     b = 0
@@ -336,7 +348,8 @@ if __name__ == '__main__':
     plt.ylabel("loss")
     plt.show()
     '''
-    grad_descent(W,b,train_X,trainTarget,val_X,validTarget,test_X,testTarget, alpha=0.005,epochs=5000,reg=0,error_tol=0.0000001)
+    grad_descent(W, b, train_X, trainTarget, val_X, validTarget, test_X,
+                 testTarget, alpha=0.005, epochs=5000, reg=0, error_tol=0.0000001)
     #new_W, new_b = grad_descent(W,b,train_X,y=trainTarget,alpha=0.005,epochs=5000,reg=0,error_tol=0.0000001)
-    #mse()
-    #return
+    # mse()
+    # return
